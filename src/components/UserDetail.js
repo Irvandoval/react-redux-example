74c5e8137bfe849e516e8f64a69eb54e1c7aefdb
Irvin Sandoval <irvandoval@gmail.com>
2017-12-19 00:01:58 -0600

    ignore package-lock.json

--- src/components/UserDetail.js
+++ src/components/UserDetail.js
@@ -5,6 +5,7 @@ const UserDetail = ({ user }) => {
   if (!user) return <h2>Search a user</h2>;
   return (<Thumbnail src={user.avatar_url} alt="242x200" className="text-center">
     <h3>{user.name}</h3>
+    <p>{user.login}</p>
     <h4>{user.bio}</h4>
     <h5>{user.email}</h5>
     <a className="btn btn-primary" href={user.html_url}>
